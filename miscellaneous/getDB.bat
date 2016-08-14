ff7b27b741c653f6c349a4d162eebe198d5eeb79
Riyaan <riyaan.williams@gmail.com>
2016-08-14 11:52:39 +0200

    Added bat file to get store a local copy of the DB

---
+++ miscellaneous/getDB.bat
@@ -0,0 +1,7 @@
+C:\Users\PhpDev\AppData\Local\Android\Sdk\platform-tools\adb shell run-as com.photoshotlist chmod 777 /data/data/com.photoshotlist/databases/
+
+C:\Users\PhpDev\AppData\Local\Android\Sdk\platform-tools\adb shell run-as com.photoshotlist chmod 777 /data/data/com.photoshotlist/databases/PhotoShotList
+
+C:\Users\PhpDev\AppData\Local\Android\Sdk\platform-tools\adb shell cp /data/data/com.photoshotlist/databases/PhotoShotList /sdcard/
+
+C:\Users\PhpDev\AppData\Local\Android\Sdk\platform-tools\adb pull /sdcard/PhotoShotList
