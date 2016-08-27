9704803f0cff9018d7a40892e752ba9197729c82
Riyaan <riyaan.williams@gmail.com>
2016-08-27 11:03:19 +0200

    Added action items

--- app/src/main/java/com/photoshotlist/MainActivity.java
+++ app/src/main/java/com/photoshotlist/MainActivity.java
@@ -6,6 +6,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +17,7 @@ import com.photoshotlist.dal.ShotListDAO;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     public final static String EXTRA_MESSAGE = "YANIE";
 
     @Override
@@ -21,13 +25,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        PSLDatabaseHelper dbHelper = PSLDatabaseHelper.getInstance(this);
-//        try {
-//            ShotListDAO dao = dbHelper.GetShotListByName("test");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void onClickAddCategory(View view)
     {
